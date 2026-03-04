e74c1d5e30577ef535ac429e3a11bff6c8421620
Kyle Robinson <ky.robinson72@gmail.com>
2026-03-03 17:30:21 -0700

    Created index.jsx and app.jsx to help generate and render new material on the initial screen

---
+++ frontend/src/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to FormForge</h1>
+      <h2>Build and customize a form for any of your needs</h2>
+    </div>
+  );
+}
