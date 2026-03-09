8169577e8dde63fdfc90e804fd9825053f48948d
Kyle Robinson <ky.robinson72@gmail.com>
2026-03-08 19:10:39 -0600

    Updated app.jsx to use BrowserRouter and Routes

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import Form from "./new_form";
+import Home from "./home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import "./app.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Welcome to FormForge</h1>
-      <h2>Build and customize a form for any of your needs</h2>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/new-form" element={<Form />}></Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      404: Return to sender. Address unknown.
+    </main>
   );
 }
